4efc65a5c2cf8f929965cc1b9e45faadebd54f47
Meitar Moscovitz <meitarm@gmail.com>
2016-07-20 20:20:49 -0600

    Initial commit forked from Danielle Sucher's "Jailbreak the Patriarchy."

---
+++ README.txt
@@ -0,0 +1,37 @@
+Chrome extension to de-euphemize certain common phrases and other terms throughout web browsing. For instance:
+
+* "Police officers" becomes "Slave patrollers."
+* "Police violence" becomes "lynchings."
+
+You get the idea. :) It also adds a "toggle euphemisms" browser action button that reloads the current tab and pauses/unpauses the extension.
+
+Please suggest new phrases and their less euphemistic counterparts!
+
+* Long-form suggestions and bug reports: https://github.com/meitar/de-euphemizer/issues/new
+* You can also tweet your suggestions @maymaymx.
+
+HAVE WE DISARMED THE POLICE YET?
+https://maymay.net/have-we-disarmed-the-police-yet/
+
+***
+
+INSTALL INSTRUCTIONS:
+
+1. Download the latest version as a ZIP file: https://github.com/meitar/de-euphemizer/archive/master.zip
+2. Unzip the ZIP file you downloaded.
+3. Open Google Chrome and go to chrome://extensions/ (in the address bar).
+4. Tick the "Developer Mode" checkbox. The "Load unpacked extension…" button appears.
+5. Click the "Load unpacked extension…" button.
+6. Browse to the folder you unzipped in step 2.
+
+I'm in the process of getting Google to publish this on their Chrome Web Store, but that's not done yet.
+
+***
+
+CREDITS:
+
+The core of this extension is based on Danielle Sucher's wonderful "Jailbreak the Patriarchy" extension.
+http://www.daniellesucher.com/2011/11/jailbreak-the-patriarchy-my-first-chrome-extension/
+
+Thanks also to @BlackAutonomist and @AyoMalireid for the inspiration:
+https://twitter.com/BlackAutonomist/status/755743951177584640
---
+++ myscript.js
@@ -0,0 +1,96 @@
+var complete = {
+};
+
+var start =  {
+};
+
+var anywhere = {
+  "police officer": "slave patroller",
+  "police officers": "slave patrollers",
+  "police man": "slave patroller",
+  "police men": "slave patrollers",
+  "police woman": "slave patroller",
+  "police women": "slave patrollers",
+  "police violence": "lynchings",
+  "policeman": "slave patroller",
+  "policemen": "slave patrollers",
+  "policewoman": "slave patroller",
+  "policewomen": "slave patrollers",
+  "republican": "fascist",
+  "republicanism": "fascism",
+};
+
+var end = {
+};
+
+var map = {};
+var maps = [ start, end, complete, anywhere ];
+for (var i = 0; i < maps.length; i++) {
+  for (attr in maps[i]) {
+    map[attr] = maps[i][attr];
+  }
+}
+
+
+var concatString = function(obj) {
+  var parts = [];
+  for (key in obj) {
+    parts.push(key);
+  }
+  return parts.join('|');
+};
+
+//var regex = '^(' + concatString(start) + ')|(' + concatString(end) + ')$|(' + concatString(anywhere) + ')|^(' + concatString(complete) + ')$';
+var regex = '(' + concatString(anywhere) + ')';
+
+var searchFor = new RegExp(regex, 'i');
+
+function capitalize(word) {
+  var first = word.charAt(0);
+  var rest = word.slice(1);
+
+  return first.toUpperCase() + rest.toLowerCase();
+}
+
+function matchCase(old_word, replacement) {
+  if (replacement.toLowerCase() == old_word.toLowerCase()) return old_word;
+
+  var first = old_word.charAt(0);
+  var second = old_word.charAt(1);
+
+  if (/[a-z]/.test(first)) return replacement.toLowerCase();
+  if (/[A-Z]/.test(second)) return replacement.toUpperCase();
+
+  return capitalize(replacement);
+}
+
+function findMatch(word) {
+  return map[word];
+}
+
+function euphemismSwap(text) {
+  return matchCase(text, text.toLowerCase().replace(searchFor, findMatch));
+}
+
+function jailbreak(node){
+  var treeWalker = document.createTreeWalker(
+      node,
+      NodeFilter.SHOW_TEXT,
+      null,
+      false
+  );
+  while(treeWalker.nextNode()) {
+    var current = treeWalker.currentNode;
+    current.textContent = euphemismSwap(current.textContent);
+  }
+}
+
+chrome.runtime.sendMessage({name: "isPaused?"}, function(response) {
+  if (response.value != 'true') {
+    jailbreak(document.body);
+
+    document.body.addEventListener('DOMNodeInserted', function(event) {
+        jailbreak(event.target);
+    });
+  }
+});
